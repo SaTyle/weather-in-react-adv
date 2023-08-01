2b73ccaadbe434516dfeb307cb212b19f8a2d85a
SaTyle <zxcvwifi@gmail.com>
2023-08-01 19:00:37 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,7 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "df46425ea4f4810380d663cb29f008dd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+// weather_api_key = "df46425ea4f4810380d663cb29f008dd" 
+// weather_api_key = "f876dfc52997ffe1b09fa00e6a5d55b2"
